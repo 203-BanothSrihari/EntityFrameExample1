51a04dbb9a19b80990126880371fe8bf150a276b
sriharibanoth <sreeharibanoth1203@gmail.com>
2022-01-05 10:55:43 +0530

    Add project files.

---
+++ EntityFrameExample1/Data/EntityFrameExample1Context.cs
@@ -0,0 +1,19 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+using Microsoft.EntityFrameworkCore;
+using EntityFrameExample1.Models;
+
+namespace EntityFrameExample1.Data
+{
+    public class EntityFrameExample1Context : DbContext
+    {
+        public EntityFrameExample1Context (DbContextOptions<EntityFrameExample1Context> options)
+            : base(options)
+        {
+        }
+
+        public DbSet<EntityFrameExample1.Models.Employee> Employee { get; set; }
+    }
+}
---
+++ EntityFrameExample1/Models/Employee.cs
@@ -0,0 +1,20 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+
+namespace EntityFrameExample1.Models
+{
+    public class Employee
+    {
+        public int ID { get; set; }
+
+        public string Name { get; set; }
+
+        public double Salary { get; set; }
+
+        public string Location { get; set; }
+
+
+    }
+}
